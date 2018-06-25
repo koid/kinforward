a30da56dee1689d033e116ecf21f3e8d3c331d09
koid <yukinori.koide@gunosy.com>
2018-06-25 12:00:12 +0900

    return if dogstatsd client is nil

--- dogstatsd/dogstatsd.go
+++ dogstatsd/dogstatsd.go
@@ -27,14 +27,21 @@ func New(addr string, tags []string) (*Statsd, error) {
 
 // Checkpoint implementation that writes to statsd
 func (s *Statsd) Checkpoint() {
+	if s.client == nil {
+		return
+	}
+
 	_ = s.client.Incr("kinsumer.checkpoints", s.tags, 1.0)
 }
 
 // EventToClient implementation that writes to statsd metrics about a record
 // that was consumed by the client
 func (s *Statsd) EventToClient(inserted, retrieved time.Time) {
-	now := time.Now()
+	if s.client == nil {
+		return
+	}
 
+	now := time.Now()
 	_ = s.client.Incr("kinsumer.consumed", s.tags, 1.0)
 	_ = s.client.Timing("kinsumer.in_stream", retrieved.Sub(inserted), s.tags, 1.0)
 	_ = s.client.Timing("kinsumer.end_to_end", now.Sub(inserted), s.tags, 1.0)
@@ -44,6 +51,10 @@ func (s *Statsd) EventToClient(inserted, retrieved time.Time) {
 // EventsFromKinesis implementation that writes to statsd metrics about records that
 // were retrieved from kinesis
 func (s *Statsd) EventsFromKinesis(num int, shardID string, lag time.Duration) {
+	if s.client == nil {
+		return
+	}
+
 	shardTag := fmt.Sprintf("shardId:%s", shardID)
 	_ = s.client.Timing("kinsumer.lag", lag, append(s.tags, shardTag), 1.0)
 	_ = s.client.Count("kinsumer.retrieved", int64(num), append(s.tags, shardTag), 1.0)
